e50886da67c4f1ef59c826bc1c524861c58db7f8
Bertrand Killian <killianzazou13@gmail.com>
2026-04-29 22:26:39 +0200

    Added the vertical transition type corridors enum from issue #18

---
+++ Generation/VerticalTransitionType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBindingOfV1.Generation
+{
+    public enum VerticalTransitionType
+    {
+        Vent,
+        WallJump,
+        JumpPad,
+        FreeFall,
+        Elevator,
+        DirectFall     // non-adjacent stacked rooms
+    }
+}
